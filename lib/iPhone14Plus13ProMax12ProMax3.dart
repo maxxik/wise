9d61fd9b2d1f02be86ef8a97d64f590965fd7746
maxxik <maxikka@gmail.com>
2022-10-21 23:35:59 +0200

    init

---
+++ lib/iPhone14Plus13ProMax12ProMax3.dart
@@ -0,0 +1,422 @@
+import 'package:flutter/material.dart';
+import 'package:adobe_xd/pinned.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class iPhone14Plus13ProMax12ProMax3 extends StatelessWidget {
+  iPhone14Plus13ProMax12ProMax3({
+    Key key,
+  }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xffffffff),
+      body: Stack(
+        children: <Widget>[
+          Container(
+            color: const Color(0xffffffff),
+          ),
+          Pinned.fromPins(
+            Pin(start: 29.0, end: 53.0),
+            Pin(size: 40.0, start: 82.0),
+            child: Text(
+              'Holiday -Croatia',
+              style: TextStyle(
+                fontFamily: 'Averta ☞',
+                fontSize: 30,
+                color: const Color(0xff37517e),
+                letterSpacing: -0.3,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          Pinned.fromPins(
+            Pin(start: 35.0, end: 47.0),
+            Pin(size: 33.0, middle: 0.3841),
+            child: Text(
+              'Records',
+              style: TextStyle(
+                fontFamily: 'Averta ☞',
+                fontSize: 25,
+                color: const Color(0xff37517e),
+                letterSpacing: -0.25,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          Pinned.fromPins(
+            Pin(size: 258.0, start: 53.0),
+            Pin(size: 37.0, middle: 0.4466),
+            child: Text(
+              'Holiday - Croatia',
+              style: TextStyle(
+                fontFamily: 'Averta ☞',
+                fontSize: 28,
+                color: const Color(0xffffffff),
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          Pinned.fromPins(
+            Pin(size: 19.9, start: 35.0),
+            Pin(size: 18.6, start: 29.0),
+            child: SvgPicture.string(
+              _svg_pzaz2,
+              allowDrawingOutsideViewBox: true,
+              fit: BoxFit.fill,
+            ),
+          ),
+          Pinned.fromPins(
+            Pin(size: 19.9, start: 35.0),
+            Pin(size: 18.6, start: 29.0),
+            child: SvgPicture.string(
+              _svg_rpp0pa,
+              allowDrawingOutsideViewBox: true,
+              fit: BoxFit.fill,
+            ),
+          ),
+          Pinned.fromPins(
+            Pin(size: 105.0, start: 59.0),
+            Pin(size: 33.0, middle: 0.5028),
+            child: Text(
+              '5000 €',
+              style: TextStyle(
+                fontFamily: 'Averta ☞',
+                fontSize: 25,
+                color: const Color(0xffffffff),
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment(0.303, 0.006),
+            child: SizedBox(
+              width: 72.0,
+              height: 31.0,
+              child: Text(
+                '36%',
+                style: TextStyle(
+                  fontFamily: 'Averta Demo',
+                  fontSize: 23,
+                  color: const Color(0xffffffff),
+                ),
+              ),
+            ),
+          ),
+          Pinned.fromPins(
+            Pin(start: 37.0, end: 36.0),
+            Pin(size: 41.5, middle: 0.208),
+            child: Stack(
+              children: <Widget>[
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30.0),
+                    border:
+                        Border.all(width: 1.0, color: const Color(0xff3675bd)),
+                  ),
+                  margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 0.5),
+                ),
+                Pinned.fromPins(
+                  Pin(size: 91.0, start: 0.0),
+                  Pin(start: 0.0, end: 0.5),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: const Color(0xff3675bd),
+                      borderRadius: BorderRadius.circular(21.0),
+                    ),
+                  ),
+                ),
+                Pinned.fromPins(
+                  Pin(size: 111.0, start: 16.0),
+                  Pin(start: 0.0, end: 0.5),
+                  child: Container(
+                    color: const Color(0xff3675bd),
+                  ),
+                ),
+                Pinned.fromPins(
+                  Pin(size: 1.0, middle: 0.5141),
+                  Pin(start: 0.5, end: 0.0),
+                  child: SvgPicture.string(
+                    _svg_ksl4f,
+                    allowDrawingOutsideViewBox: true,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                Pinned.fromPins(
+                  Pin(size: 1.0, middle: 0.3856),
+                  Pin(start: 0.5, end: 0.0),
+                  child: SvgPicture.string(
+                    _svg_te3ugx,
+                    allowDrawingOutsideViewBox: true,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                Pinned.fromPins(
+                  Pin(size: 1.0, middle: 0.6427),
+                  Pin(start: 0.5, end: 0.0),
+                  child: SvgPicture.string(
+                    _svg_ue4xz,
+                    allowDrawingOutsideViewBox: true,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                Pinned.fromPins(
+                  Pin(size: 1.0, middle: 0.7712),
+                  Pin(start: 0.5, end: 0.0),
+                  child: SvgPicture.string(
+                    _svg_x9rlz5,
+                    allowDrawingOutsideViewBox: true,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                Pinned.fromPins(
+                  Pin(size: 1.0, end: 35.5),
+                  Pin(start: 0.5, end: 0.0),
+                  child: SvgPicture.string(
+                    _svg_dpbkkz,
+                    allowDrawingOutsideViewBox: true,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                Pinned.fromPins(
+                  Pin(size: 1.0, middle: 0.2571),
+                  Pin(start: 0.5, end: 0.0),
+                  child: SvgPicture.string(
+                    _svg_byx4ej,
+                    allowDrawingOutsideViewBox: true,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+                Pinned.fromPins(
+                  Pin(size: 1.0, start: 45.5),
+                  Pin(start: 0.5, end: 0.0),
+                  child: SvgPicture.string(
+                    _svg_ulkxhx,
+                    allowDrawingOutsideViewBox: true,
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Pinned.fromPins(
+            Pin(size: 138.0, start: 53.0),
+            Pin(size: 21.0, start: 156.0),
+            child: Text(
+              '1800/5000 € ',
+              style: TextStyle(
+                fontFamily: 'Averta Demo',
+                fontSize: 16,
+                color: const Color(0xff37517e),
+              ),
+            ),
+          ),
+          Pinned.fromPins(
+            Pin(size: 100.0, end: 36.0),
+            Pin(size: 27.0, middle: 0.2736),
+            child: Text(
+              '+ more',
+              style: TextStyle(
+                fontFamily: 'Averta Demo',
+                fontSize: 20,
+                color: const Color(0xff37517e),
+              ),
+              textAlign: TextAlign.center,
+            ),
+          ),
+          Pinned.fromPins(
+            Pin(start: 35.0, end: 36.0),
+            Pin(size: 102.0, middle: 0.4903),
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color(0xff008ec0),
+                borderRadius: BorderRadius.circular(13.0),
+              ),
+            ),
+          ),
+          Pinned.fromPins(
+            Pin(start: 36.0, end: 35.0),
+            Pin(size: 102.0, middle: 0.6481),
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color(0xff008ec0),
+                borderRadius: BorderRadius.circular(13.0),
+              ),
+            ),
+          ),
+          Pinned.fromPins(
+            Pin(start: 36.0, end: 35.0),
+            Pin(size: 102.0, middle: 0.807),
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color(0xff008ec0),
+                borderRadius: BorderRadius.circular(13.0),
+              ),
+            ),
+          ),
+          Pinned.fromPins(
+            Pin(size: 45.8, start: 48.0),
+            Pin(size: 40.2, middle: 0.4853),
+            child: SvgPicture.string(
+              _svg_z93o0t,
+              allowDrawingOutsideViewBox: true,
+              fit: BoxFit.fill,
+            ),
+          ),
+          Align(
+            alignment: Alignment(-0.125, -0.038),
+            child: SizedBox(
+              width: 172.0,
+              height: 24.0,
+              child: Text(
+                'MCD Astoria',
+                style: TextStyle(
+                  fontFamily: 'Averta Demo',
+                  fontSize: 18,
+                  color: const Color(0xffffffff),
+                  letterSpacing: -0.18,
+                ),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment(-0.125, 0.002),
+            child: SizedBox(
+              width: 172.0,
+              height: 20.0,
+              child: Text(
+                'Hospitality',
+                style: TextStyle(
+                  fontFamily: 'Averta Demo',
+                  fontSize: 15,
+                  color: const Color(0xffffffff),
+                  letterSpacing: -0.15,
+                ),
+              ),
+            ),
+          ),
+          Align(
+            alignment: Alignment(-0.125, 0.042),
+            child: SizedBox(
+              width: 172.0,
+              height: 20.0,
+              child: Text(
+                '2022.10.21.',
+                style: TextStyle(
+                  fontFamily: 'Averta Demo',
+                  fontSize: 15,
+                  color: const Color(0xffffffff),
+                  letterSpacing: -0.15,
+                ),
+              ),
+            ),
+          ),
+          Pinned.fromPins(
+            Pin(size: 111.0, end: 47.0),
+            Pin(size: 21.0, middle: 0.4917),
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color(0xffffffff),
+                borderRadius: BorderRadius.circular(13.0),
+              ),
+            ),
+          ),
+          Pinned.fromPins(
+            Pin(size: 111.0, end: 47.0),
+            Pin(size: 21.0, middle: 0.6354),
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color(0xffffffff),
+                borderRadius: BorderRadius.circular(13.0),
+              ),
+            ),
+          ),
+          Pinned.fromPins(
+            Pin(size: 111.0, end: 47.0),
+            Pin(size: 21.0, middle: 0.7746),
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color(0xffffffff),
+                borderRadius: BorderRadius.circular(13.0),
+              ),
+            ),
+          ),
+          Pinned.fromPins(
+            Pin(start: 0.0, end: 0.0),
+            Pin(size: 72.0, end: 0.0),
+            child: Container(
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: const AssetImage(''),
+                  fit: BoxFit.fill,
+                ),
+              ),
+            ),
+          ),
+          Pinned.fromPins(
+            Pin(size: 83.0, end: 59.0),
+            Pin(size: 17.0, middle: 0.4917),
+            child: Text(
+              'Saved  2.94 €',
+              style: TextStyle(
+                fontFamily: 'Averta ☞',
+                fontSize: 13,
+                color: const Color(0xff37517e),
+                letterSpacing: -0.13,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          Pinned.fromPins(
+            Pin(size: 83.0, end: 59.0),
+            Pin(size: 17.0, middle: 0.6348),
+            child: Text(
+              'Saved  2.94 €',
+              style: TextStyle(
+                fontFamily: 'Averta ☞',
+                fontSize: 13,
+                color: const Color(0xff37517e),
+                letterSpacing: -0.13,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+          Pinned.fromPins(
+            Pin(size: 83.0, end: 59.0),
+            Pin(size: 17.0, middle: 0.7734),
+            child: Text(
+              'Saved  2.94 €',
+              style: TextStyle(
+                fontFamily: 'Averta ☞',
+                fontSize: 13,
+                color: const Color(0xff37517e),
+                letterSpacing: -0.13,
+                fontWeight: FontWeight.w600,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+const String _svg_pzaz2 =
+    '<svg viewBox="35.0 29.0 19.9 18.6" ><path transform="translate(35.0, 29.0)" d="M 0 0 L 19.91960525512695 18.63446998596191" fill="none" stroke="#008ec0" stroke-width="3" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';
+const String _svg_rpp0pa =
+    '<svg viewBox="35.0 29.0 19.9 18.6" ><path transform="translate(35.0, 29.0)" d="M 19.91960525512695 0 L 0 18.63446998596191" fill="none" stroke="#008ec0" stroke-width="3" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';
+const String _svg_ksl4f =
+    '<svg viewBox="219.0 157.5 1.0 41.0" ><path transform="translate(219.0, 157.5)" d="M 0 0 L 0 41" fill="none" fill-opacity="0.28" stroke="#920000" stroke-width="1" stroke-opacity="0.28" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';
+const String _svg_te3ugx =
+    '<svg viewBox="173.5 157.5 1.0 41.0" ><path transform="translate(173.5, 157.5)" d="M 0 0 L 0 41" fill="none" fill-opacity="0.28" stroke="#920000" stroke-width="1" stroke-opacity="0.28" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';
+const String _svg_ue4xz =
+    '<svg viewBox="264.5 157.5 1.0 41.0" ><path transform="translate(264.5, 157.5)" d="M 0 0 L 0 41" fill="none" fill-opacity="0.28" stroke="#920000" stroke-width="1" stroke-opacity="0.28" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';
+const String _svg_x9rlz5 =
+    '<svg viewBox="310.0 157.5 1.0 41.0" ><path transform="translate(310.0, 157.5)" d="M 0 0 L 0 41" fill="none" fill-opacity="0.28" stroke="#920000" stroke-width="1" stroke-opacity="0.28" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';
+const String _svg_dpbkkz =
+    '<svg viewBox="355.5 157.5 1.0 41.0" ><path transform="translate(355.5, 157.5)" d="M 0 0 L 0 41" fill="none" fill-opacity="0.28" stroke="#920000" stroke-width="1" stroke-opacity="0.28" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';
+const String _svg_byx4ej =
+    '<svg viewBox="128.0 157.5 1.0 41.0" ><path transform="translate(128.0, 157.5)" d="M 0 0 L 0 41" fill="none" fill-opacity="0.28" stroke="#920000" stroke-width="1" stroke-opacity="0.28" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';
+const String _svg_ulkxhx =
+    '<svg viewBox="82.5 157.5 1.0 41.0" ><path transform="translate(82.5, 157.5)" d="M 0 0 L 0 41" fill="none" fill-opacity="0.28" stroke="#920000" stroke-width="1" stroke-opacity="0.28" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';
+const String _svg_z93o0t =
+    '<svg viewBox="48.0 429.9 45.8 40.2" ><path transform="translate(47.28, 427.09)" d="M 46.45476913452148 13.54922771453857 L 42.69717025756836 43.00457763671875 L 30.00537872314453 43.00457763671875 L 26.2630615234375 13.4021692276001 L 41.75753402709961 13.4021692276001 L 44.81995010375977 2.824559211730957 L 46.55598831176758 3.347854614257812 L 43.61771392822266 13.52726364135742 L 46.45477294921875 13.54922676086426 Z M 24.46876907348633 30.59066200256348 C 24.46876907348633 30.59066200256348 24.94336318969727 26.77099990844727 18.3620777130127 26.77099990844727 L 7.880918502807617 26.77099990844727 C 1.309183359146118 26.77099990844727 1.774227499961853 30.59066581726074 1.774227499961853 30.59066581726074 L 24.46876907348633 30.59066581726074 Z M 1.774227738380432 39.18490982055664 C 1.774227738380432 39.18490982055664 1.309183359146118 43.00457763671875 7.880918502807617 43.00457763671875 L 18.36303520202637 43.00457763671875 C 24.9443187713623 43.00457763671875 24.4697265625 39.18490600585938 24.4697265625 39.18490600585938 L 1.774227738380432 39.18490600585938 Z M 23.48425102233887 37.27507781982422 C 24.56139755249023 37.27507781982422 25.43132591247559 36.21129989624023 25.43132591247559 34.88874053955078 C 25.43132591247559 33.55854415893555 24.56139755249023 32.50049591064453 23.48425102233887 32.50049591064453 L 2.668030023574829 32.50049591064453 C 1.595658421516418 32.50049591064453 0.7200002670288086 33.55854415893555 0.7200002670288086 34.88874053955078 C 0.7200002670288086 36.21129989624023 1.595658421516418 37.27507781982422 2.668030023574829 37.27507781982422 L 23.48425483703613 37.27507781982422 Z" fill="#ffffff" stroke="none" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" /></svg>';

5898c629e2fb9a541acb14a103b842bda6d46f7a
maxxik <maxikka@gmail.com>
2022-10-23 10:00:16 +0200

    final

--- README.md
+++ README.md
@@ -1,16 +1,8 @@
-# wise
+Our application is a based on a mock database which can be run on android, IOS, a Windows devices.
+During the development we wanted to make sure that our application can be integrated into the Wise design language.
+Within the application the user can open savings accounts, manage them, activate and deactivate the feature all together.
+Users can get information on there progress and the effectiveness of their saving.In the App users can get summeries of their spendings and can choose
+which of these categories would contribute to and in what proportion to our savings.The application is adaptive and to the customers needs and enables 
+the user to save their money in manageable proportions in an easy manner.
 
-A new Flutter project.
-
-## Getting Started
-
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+Settings and statistics sreens are not yet fully implemented.
--- lib/StatisticsScreen.dart
+++ lib/StatisticsScreen.dart
@@ -33,6 +33,32 @@ class _StatisticsScreenState extends State<StatisticsScreen> {
           mainAxisAlignment: MainAxisAlignment.start,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
+            // Navigation buttons
+            Padding(
+              padding: const EdgeInsets.fromLTRB(lP, tP, rP, 0),
+              child:
+                  // add a button to the left that says "back"
+                  Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                // set padding to 10
+
+                children: [
+                  IconButton(
+                    alignment: Alignment.centerLeft,
+                    icon: SvgPicture.string(
+                      '<svg viewBox="32.0 27.1 23.0 22.4" ><path transform="translate(31.96, 24.48)" d="M 13.1943244934082 23.52655029296875 L 12.05671215057373 24.66416168212891 C 11.5750207901001 25.14585494995117 10.79611492156982 25.14585494995117 10.31954765319824 24.66416168212891 L 0.3577531576156616 14.70749378204346 C -0.1239385381340981 14.22580146789551 -0.1239385381340981 13.44689750671387 0.3577531576156616 12.97032833099365 L 10.31954765319824 3.00853443145752 C 10.80123901367188 2.526842594146729 11.58014488220215 2.526842594146729 12.05671215057373 3.00853443145752 L 13.1943244934082 4.146146774291992 C 13.6811408996582 4.632962703704834 13.67089176177979 5.427241802215576 13.17382717132568 5.903809070587158 L 6.998950004577637 11.78659725189209 L 21.72641754150391 11.78659725189209 C 22.407958984375 11.78659725189209 22.95626831054688 12.33490657806396 22.95626831054688 13.01644897460938 L 22.95626831054688 14.65625 C 22.95626831054688 15.33779239654541 22.407958984375 15.88610172271729 21.72641754150391 15.88610172271729 L 6.998950004577637 15.88610172271729 L 13.17382717132568 21.76889038085938 C 13.67601585388184 22.24545669555664 13.68626499176025 23.03973388671875 13.1943244934082 23.52655029296875 Z" fill="#00b9ff" stroke="none" stroke-width="1" stroke-miterlimit="4" stroke-linecap="butt" /></svg>',
+                      allowDrawingOutsideViewBox: true,
+                      fit: BoxFit.fill,
+                      height: 23,
+                    ),
+                    onPressed: () {
+                      // go back to the previous screen
+                      Navigator.pop(context);
+                    },
+                  ),
+                ],
+              ),
+            ),
             // image add
             Image.asset(
               "assets/images/plot_cum.png",
